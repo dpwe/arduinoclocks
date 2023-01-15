c6c2187c9b79943128ed8f3ca7cefd9334191145
Dan Ellis <dan.ellis@gmail.com>
2023-01-14 23:49:40 -0500

    DS3231_emulator: Fixes and updates back-ported from integrated Emulator-explorer.

--- DS3231_emulator/DS3231_emulator.ino
+++ DS3231_emulator/DS3231_emulator.ino
@@ -87,7 +87,7 @@ void ARDUINO_ISR_ATTR onTimer(){
   // Setup for next alarm, including cumulated nanos.
   cumulated_nanos += nanosecs_per_sec_trim;
   int micros_offset = cumulated_nanos / 1000;
-  cumulated_nanos += micros_offset * 1000;
+  cumulated_nanos -= micros_offset * 1000;
   timerAlarmWrite(timer, counter_ticks_between_firings + micros_offset, true);
 }
 
@@ -115,6 +115,8 @@ void timer_setup(void) {
 void timer_reset_sync(void) {
   // Happens e.g. when seconds register is written.  Make seconds happen relative to now.
   timerWrite(timer, 0);
+  // Update output state.
+  clock_tick();
 }
 
 // ----- DS3231 emulation -----
@@ -131,17 +133,6 @@ uint8_t registers1[NUM_REGISTERS];
 // Current register set.
 uint8_t *registers = registers0;
 uint8_t *registers_next = registers1;
-
-void ds3231_setup() {
-  // Zero-out registers.
-  for (int i = 0; i < NUM_REGISTERS; ++i) {
-    registers[i] = 0;
-    registers_next[i] = 0;
-  }
-  // Initialize temperature to something plausible, 25.0 C
-  registers[DS3231_TEMPERATUREREG] = 25;
-  registers_next[DS3231_TEMPERATUREREG] = 25;
-}
 
 class DateTime decode_time_from_regs(uint8_t *buffer) {
   // Convert ds3231 time registers to Unix time.
@@ -161,6 +152,22 @@ void encode_time_to_regs(const DateTime& dt, uint8_t *buffer) {
   buffer[4] = bin2bcd(dt.day());
   buffer[5] = bin2bcd(dt.month());
   buffer[6] = bin2bcd(dt.year() - 2000U);
+}
+
+void ds3231_setup() {
+  // Zero-out registers.
+  for (int i = 0; i < NUM_REGISTERS; ++i) {
+    registers[i] = 0;
+  }
+  // Initialize time to something legal.
+  encode_time_to_regs(DateTime(2000, 1, 1, 0, 0, 0), registers);
+  // Initialize temperature to something plausible, 25.0 C
+  registers[DS3231_TEMPERATUREREG] = 25;
+  
+  // Copy to 2nd registers.
+  for (int i = 0; i < NUM_REGISTERS; ++i) {
+    registers_next[i] = registers[i];
+  }
 }
 
 class DateTime decode_alarm(uint8_t *buffer, uint8_t alarm_num, uint8_t *p_alarm_mode) {
@@ -240,10 +247,11 @@ bool check_alarm_match(DateTime &now, DateTime &alarm, uint8_t alarm_mode) {
 
 #define CHECK_BIT(reg, bit) (reg & _BV(bit))
 
-void ds3231_tick(uint8_t *registers) {
-  // Add one second to the internal clock.
+void ds3231_tick(uint8_t *registers, uint8_t advance=1) {
+  // Add one second (by default) to the internal clock.
+  // We also allow advance=0 to simply recalculate state (alarm outputs) without advancing clock.
   uint32_t time = decode_time_from_regs(registers).unixtime();
-  time += 1;
+  time += advance;
   DateTime now(time);
   encode_time_to_regs(now, registers);
   
@@ -273,67 +281,33 @@ void ds3231_tick(uint8_t *registers) {
       registers[DS3231_OUTPINSTATE] = HIGH;
     }
   }
+  // Update the trim by 10 ppb per aging offset.  Positive aging offset makes clock slower.
+  // Maybe we should only do this when it is changed, i.e. check writes to AGING register?
+  timer_update_trim_ppb(10 * (int)((int8_t)registers[DS3231_AGING]));
 }
 
-void ds3231_setup_next_tick(void) {
+void ds3231_setup_next_tick(int advance=1) {
   // Setup registers_next to be correct for the next tick event.
   // First, copy current registers to registers_next:
   for (int i = 0; i < NUM_REGISTERS; ++i) {
     registers_next[i] = registers[i];
   }
   // Then, advance it by 1 second.
-  ds3231_tick(registers_next);
+  ds3231_tick(registers_next, advance);
 }
 
-
-// --------- I2C handlers --------------
-// We need to track that the registers have been updated and may not yet
-// have propagated to registers_next.
-volatile bool registers_dirty = false;
-
-uint8_t cursor = 0;  // Address of next register access.
-
-void receiveEvent(int howmany) {
-  // Assume we got at least one data byte, and it's the cursor.
-  cursor = Wire.read();
-
-  // Any subsequent bytes are writes to that register.
-  while (Wire.available()) {
-    uint8_t x = Wire.read();
-    if (cursor < NUM_REGISTERS) {
-      registers[cursor] = x;
-      if (cursor == 0) {
-        // We wrote the seconds register, reset the timer sync.
-        timer_reset_sync();
-      }
-    }
-    ++cursor;
+void ds3231_registers_updated(bool seconds_modified) {
+  // Notification that the registers were modified externally (e.g. by I2C write).
+  // Behavior varies depending on whether seconds were set, in which case the 
+  // ticking changes phase.
+  // In other situation, let the seconds advance at the next tick.
+  ds3231_setup_next_tick(/* advance= */ seconds_modified ? 0 : 1);
+  // Need to reset sync if we wrote seconds.
+  if (seconds_modified) {
+    timer_reset_sync();  // Will deploy the registers_next setup by setup_next_tick.
   }
-  registers_dirty = true;
 }
 
-void requestEvent() {
-  // Send a stream of bytes back to master, starting from previous value of cursor until end of registers.
-  // I *think* I2C indicates how many bytes it wants by sending a STOP after it's had enough.
-  // but this doesn't appear to be visible through Wire.
-  // So we send everything, and count on any excess being dropped.
-  uint8_t x = 0;
-#ifdef ONEBYONE
-  while (cursor < NUM_REGISTERS) {
-    x = registers[cursor];
-    Serial.print("Send Reg[");
-    Serial.print(cursor);
-    Serial.print("]=");
-    Serial.println(x);
-    Wire.write(x);
-    ++cursor;
-  }
-#else  // Use block write.
-  //Serial.print("Send regs from ");
-  //Serial.println(cursor);
-  Wire.write(registers + cursor, NUM_REGISTERS - cursor);
-#endif
-}
 
 // ------- Tick interrupt -------
 
@@ -344,35 +318,70 @@ const uint32_t sqwv_pulse_ms = 500;
 
 // Detect a tick in foreground.
 volatile bool tick_happened = false;
+// Just to help interface with explorer
+volatile uint32_t rtc_micros = 0;
 
 void clock_tick(void) {
-  if (registers_dirty) {
-    // Registers have been modified externally e.g. by i2c_write.
-    // This is normally handled by the foreground task and we specifically don't want to 
-    // do it here, to avoid latency between clock tick and state update.  However, if the
-    // registers *just* got changed by an i2c_write, and we haven't yet propagated to 
-    // registers_next, we need to take care of that here - or we'll lose the register updates.
-    ds3231_setup_next_tick();
-    registers_dirty = false;
-  }
   // Swap the registers double-buffer.  Do this and update output pin as fast as possible.
   uint8_t *tmp = registers;
   registers = registers_next;
+  registers_next = tmp;
   // Update the output pin.
   digitalWrite(SQWV_PIN, registers[DS3231_OUTPINSTATE]);
-  registers_next = registers;
-  // Copy the new registers as setup for the next tick.
-  // Record the tick
-  tick_happened = true;
   if (!CHECK_BIT(registers[DS3231_CONTROL], DS3231_C_INTCN)) {
     // Set up semaphore for the oscillator pulse to be reset later.
     last_sqwv_millis = millis();
   }
-  // Update the trim.
-  timer_update_trim_ppb((int8_t)registers[DS3231_AGING]);
+  // Record the tick
+  tick_happened = true;
+  // To let explorer know that this happened.
+  rtc_micros = micros();
 }
 
+
+// --------- I2C Delegate handlers (on Wire1) --------------
+
+uint8_t cursor = 0;  // Address of next register access.
+
+void receiveEvent(int howmany) {
+  // Assume we got at least one data byte, and it's the cursor.
+  bool seconds_modified = false;
+  cursor = Wire1.read();
+
+  // Any subsequent bytes are writes to that register.
+  while (Wire1.available()) {
+    uint8_t x = Wire1.read();
+    if (cursor < NUM_REGISTERS) {
+      registers[cursor] = x;
+      if (cursor == 0) {
+        // We wrote the seconds register, reset the timer sync.
+        seconds_modified = true;
+      }
+    }
+    ++cursor;
+  }
+  // Maybe act on the new register values.
+  ds3231_registers_updated(seconds_modified);  
+}
+
+void requestEvent() {
+  // Send a stream of bytes back to master, starting from previous value of cursor until end of registers.
+  // I *think* I2C indicates how many bytes it wants by sending a STOP after it's had enough.
+  // but this doesn't appear to be visible through Wire.
+  // So we send everything, and count on any excess being dropped.
+  uint8_t x = 0;
+  //Serial.print("Send regs from ");
+  //Serial.println(cursor);
+  Wire1.write(registers + cursor, NUM_REGISTERS - cursor);
+}
+
+
 // --------------- setup(), loop() ---------------------
+
+// Delegate I2C bus config.  This acts like the DS3231 chip.
+#define SDA_PIN A4
+#define SCL_PIN A5
+#define I2C_FREQ 100000
 
 void setup() {
   // Configure SQWV output pin.
@@ -380,13 +389,11 @@ void setup() {
   digitalWrite(SQWV_PIN, HIGH);  // Default state.
   
   // Initialize I2C communications as minion
-  Wire.begin(DS3231_ADDRESS);
-  
+  Wire1.begin(DS3231_ADDRESS, SDA_PIN, SCL_PIN, I2C_FREQ);
   // Function to run when data requested from master
-  Wire.onRequest(requestEvent); 
-  
+  Wire1.onRequest(requestEvent); 
   // Function to run when data received from master
-  Wire.onReceive(receiveEvent);
+  Wire1.onReceive(receiveEvent);
   
   // Setup Serial Monitor 
   Serial.begin(9600);
@@ -412,11 +419,6 @@ void loop() {
     tick_happened = false;
     // Report seconds.
     Serial.println(registers[0], HEX);
-  }
-  if (registers_dirty) {
-    // Registers have been modified externally e.g. by i2c_write.
-    ds3231_setup_next_tick();
-    registers_dirty = false;
   }
   // Time delay in loop
   delay(10);
